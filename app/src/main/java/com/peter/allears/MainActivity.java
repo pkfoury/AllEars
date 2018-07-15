d8ccafbd96cc21554bae97714a02e79e88ca339f
Peter Kfoury <pkfoury@purdue.edu>
2018-07-15 00:44:13 -0500

    Implements notification listening

--- app/src/main/java/com/peter/allears/MainActivity.java
+++ app/src/main/java/com/peter/allears/MainActivity.java
@@ -1,19 +1,44 @@
 package com.peter.allears;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class MainActivity extends AppCompatActivity {
+    static String NOTIFICATION_CHANNEL_ID = "notif_channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
+
+        final NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        final NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+        // Configure the notification channel.
+        notificationChannel.setDescription("Channel description");
+        notificationChannel.enableLights(true);
+        notificationChannel.enableVibration(true);
+        notificationManager.createNotificationChannel(notificationChannel);
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("TestTitle")
+                .setContentText("testText")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -22,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                notificationManager.notify(1, builder.build());
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
--- app/src/main/java/com/peter/allears/NotificationListener.java
+++ app/src/main/java/com/peter/allears/NotificationListener.java
@@ -1,27 +1,34 @@
 package com.peter.allears;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 public class NotificationListener extends NotificationListenerService {
     public NotificationListener() {
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return super.onBind(intent);
+    public void onCreate() {
+        super.onCreate();
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        // do whatever
+        Log.i("Msg", sbn.getNotification().extras.getString("android.title"));
+        Log.i("Msg", sbn.getNotification().extras.getString("android.text"));
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
-        // do whatever
+        Log.i("Msg", "removed");
     }
 }
